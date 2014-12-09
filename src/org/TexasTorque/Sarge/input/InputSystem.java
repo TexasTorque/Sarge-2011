9a958e1b55a879124229064a799882b1d5b0cd81
Matthew Webb <matthewwebb65@yahoo.com>
2014-12-08 16:07:03 -0800

    added inputsystem

--- src/org/TexasTorque/Sarge/input/InputSystem.java
+++ src/org/TexasTorque/Sarge/input/InputSystem.java
@@ -1,4 +1,21 @@
 package org.TexasTorque.Sarge.input;
 
 public abstract class InputSystem {
+
+    //Drivebase
+    protected volatile double leftSpeed;
+    protected volatile double rightSpeed;
+    protected volatile boolean isHighGear;
+
+    public synchronized double getLeftSpeed() {
+        return leftSpeed;
+    }
+
+    public synchronized double getRightSpeed() {
+        return rightSpeed;
+    }
+
+    public synchronized boolean getGear() {
+        return isHighGear;
+    }
 }
