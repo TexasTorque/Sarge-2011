bcfd161b7968549293de4fd5625786625d09356f
Matthew Webb <matthewwebb65@yahoo.com>
2014-12-08 17:29:45 -0800

    added driver input channel mixing

--- src/org/TexasTorque/Sarge/input/DriverInput.java
+++ src/org/TexasTorque/Sarge/input/DriverInput.java
@@ -1,5 +1,19 @@
 package org.TexasTorque.Sarge.input;
 
-public class DriverInput {
-    
+import edu.wpi.first.wpilibj.Joystick;
+
+public class DriverInput extends InputSystem {
+
+    private final Joystick driver;
+
+    public DriverInput() {
+        driver = new Joystick(1);
+    }
+
+    public void run() {
+        double x = driver.getRawAxis(4);
+        double y = driver.getRawAxis(2);
+        leftSpeed = y + x;
+        rightSpeed = y - x;
+    }
 }
