babaa3e2bc97c9d753a52b2532626179f296537c
Gijs Landwehr <gijs@fam-landwehr.nl>
2014-12-09 10:36:54 -0600

    Fix channel mixing

--- src/org/TexasTorque/Sarge/input/DriverInput.java
+++ src/org/TexasTorque/Sarge/input/DriverInput.java
@@ -12,10 +12,12 @@ public class DriverInput extends InputSystem {
 
     public void run() {
         //Drivebase
-        double x = -driver.getRawAxis(2);
-        double y = driver.getRawAxis(5);
+        double y = -driver.getRawAxis(2);
+        double x = driver.getRawAxis(5);
+        
         leftSpeed = y + x;
         rightSpeed = y - x;
+        
         isDropCenter = driver.getRawButton(5);
         isHighGear = driver.getRawButton(6);
     }
