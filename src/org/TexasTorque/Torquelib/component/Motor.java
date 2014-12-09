d2c25cac499cc32e2644b13ef86c49fd1031aaa3
Gijs Landwehr <gijs@fam-landwehr.nl>
2014-12-09 09:55:08 -0600

    Added motor from torquelib

---
+++ src/org/TexasTorque/Torquelib/component/Motor.java
@@ -0,0 +1,35 @@
+package org.TexasTorque.Torquelib.component;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class Motor {
+
+    private SpeedController controller;
+    private boolean reverse;
+    private double previousSpeed;
+
+    public Motor(SpeedController sc, boolean rev) {
+        controller = sc;
+        reverse = rev;
+    }
+
+    public void set(double speed) {
+        if (speed != previousSpeed) {
+            if (reverse) {
+                speed *= -1;
+            }
+            controller.set(speed);
+
+            previousSpeed = speed;
+        }
+    }
+
+    static float LimitSpeed(float speed, float limit) {
+        if (Math.abs(speed) <= limit) {
+            return speed;
+        } else if (speed < 0) {
+            return -limit;
+        }
+        return limit;
+    }
+}
