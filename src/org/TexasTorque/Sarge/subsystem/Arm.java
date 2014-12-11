021003c45168cf290e5730b8e29253de686f88fb
Gijs Landwehr <gijs@fam-landwehr.nl>
2014-12-11 17:41:28 -0600

    Added actuators to arm

--- src/org/TexasTorque/Sarge/subsystem/Arm.java
+++ src/org/TexasTorque/Sarge/subsystem/Arm.java
@@ -1,15 +1,27 @@
 package org.TexasTorque.Sarge.subsystem;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Solenoid;
+import org.TexasTorque.Torquelib.component.Motor;
 
 public class Arm extends Subsystem {
 
-    private final Jaguar armMotor;
-    private final Jaguar handMotor;
+    private final Motor armMotor;
+    private final Motor handMotor;
+    
+    private DoubleSolenoid wristSolenoid;
+    private Solenoid handSolenoid;
+    
+    private double targetAngle;
+    private double armAngle;
 
     public Arm() {
-        armMotor = new Jaguar(0);
-        handMotor = new Jaguar(0);
+        armMotor = new Motor(new Jaguar(5), false);
+        handMotor = new Motor(new Jaguar(3), false);
+        
+        wristSolenoid = new DoubleSolenoid(5, 6);
+        handSolenoid = new Solenoid(1);
     }
 
     public void update() {
