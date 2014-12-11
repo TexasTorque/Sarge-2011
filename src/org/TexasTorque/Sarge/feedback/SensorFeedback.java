77c6aedefd9fe57afbc14860893707272ab15ef3
Gijs Landwehr <gijs@fam-landwehr.nl>
2014-12-11 16:39:14 -0600

    Added sensors

--- src/org/TexasTorque/Sarge/feedback/SensorFeedback.java
+++ src/org/TexasTorque/Sarge/feedback/SensorFeedback.java
@@ -1,8 +1,16 @@
 package org.TexasTorque.Sarge.feedback;
 
+import org.TexasTorque.Torquelib.component.TorquePotentiometer;
+import org.TexasTorque.Torquelib.component.TorqueQuadrature;
+
 public class SensorFeedback extends FeedbackSystem {
     
+    //Drivebase
+    private TorqueQuadrature leftDriveEncoder;
+    private TorqueQuadrature rightDriveEncoder;
     
+    //Arm
+    private TorquePotentiometer armPotentiometer;
     
     public SensorFeedback()
     {
---
+++ src/org/TexasTorque/Torquelib/component/TorqueEncoder.java
@@ -0,0 +1,40 @@
+package org.TexasTorque.Torquelib.component;
+
+import edu.wpi.first.wpilibj.CounterBase;
+
+public abstract class TorqueEncoder {
+
+    protected CounterBase encoder;
+
+    protected double previousTime;
+    protected double previousRate;
+
+    protected int currentPosition;
+    protected int previousPosition;
+
+    protected double instantRate;
+    protected double secantRate;
+    protected double acceleration;
+
+    public abstract void calc();
+
+    public void reset() {
+        encoder.reset();
+    }
+
+    public int get() {
+        return currentPosition;
+    }
+
+    public double getInstantRate() {
+        return instantRate;
+    }
+    
+    public double getSecantRate() {
+        return secantRate;
+    }
+
+    public double getAcceleration() {
+        return acceleration;
+    }
+}
