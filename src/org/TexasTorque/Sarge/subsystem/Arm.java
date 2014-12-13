5d3a7623249cc3c20b4ae5999faa540fd1c99566
Gijs Landwehr <gijs@fam-landwehr.nl>
2014-12-13 09:37:04 -0600

    Almost done with arm

--- src/org/TexasTorque/Sarge/subsystem/Arm.java
+++ src/org/TexasTorque/Sarge/subsystem/Arm.java
@@ -3,6 +3,7 @@ package org.TexasTorque.Sarge.subsystem;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.TexasTorque.Torquelib.component.Motor;
 
 public class Arm extends Subsystem {
@@ -15,17 +16,20 @@ public class Arm extends Subsystem {
     private double armAngle;
     private double armMotorSpeed;
     private double handMotorSpeed;
+    
     //Pneumatics
     private DoubleSolenoid wristSolenoid;
     private Solenoid handSolenoid;
     private boolean wristDown;
     private boolean handOpen;
+    
     //Angles
     public final static double FLOOR_ANGLE = 0.0;
     public final static double LOW_ANGLE = 0.0;
     public final static double MIDDLE_ANGLE = 0.0;
     public final static double HIGH_ANGLE = 0.0;
     public final static double RETRACT_ANGLE = 0.0;
+    
     //States
     public final static byte DOWN = 0;
     public final static byte INTAKE = 1;
@@ -72,7 +76,7 @@ public class Arm extends Subsystem {
                 wristDown = true;
                 break;
             case OUTTAKE:
-                if (previousTargetAngle == RETRACT_ANGLE) {
+                if (targetAngle == RETRACT_ANGLE) {
                     targetAngle = FLOOR_ANGLE;
                 }
 
@@ -82,7 +86,7 @@ public class Arm extends Subsystem {
                 wristDown = true;
                 break;
             case CARRY:
-                if (previousTargetAngle == FLOOR_ANGLE) {
+                if (targetAngle == FLOOR_ANGLE) {
                     targetAngle = RETRACT_ANGLE;
                 }
 
@@ -92,7 +96,7 @@ public class Arm extends Subsystem {
                 wristDown = false;
                 break;
             case PLACE:
-                if (targetAngle > FLOOR_ANGLE || previousState == CARRY) {
+                if (targetAngle > FLOOR_ANGLE && previousState == CARRY) {
                     handMotorSpeed = placePower;
 
                     handOpen = true;
@@ -100,11 +104,23 @@ public class Arm extends Subsystem {
                 }
                 break;
         }
+        
+        if (outputEnabled)
+        {
+            wristSolenoid.set((wristDown) ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
+            handSolenoid.set(handOpen);
+            
+            handMotor.set(handMotorSpeed);
+        }
 
         previousTargetAngle = targetAngle;
         previousState = state;
     }
 
     public void pushToDashboard() {
+        SmartDashboard.putBoolean("WristDown", wristDown);
+        SmartDashboard.putBoolean("HandOpen", handOpen);
+        SmartDashboard.putNumber("HandRollerSpeed", handMotorSpeed);
+        SmartDashboard.putNumber("ArmMotorSpeed", armMotorSpeed);
     }
 }
